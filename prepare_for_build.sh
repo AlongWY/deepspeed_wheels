73f5e613ad4daa145529eeb3bd99a5f6fb528d2b
Feng Yunlong <20281571+AlongWY@users.noreply.github.com>
2025-08-22 06:10:23 +0000

    compile comm ops

--- prepare_for_build.sh
+++ prepare_for_build.sh
@@ -4,6 +4,7 @@ curl https://pagure.io/libaio/archive/libaio-0.3.113/libaio-libaio-0.3.113.tar.g
 tar -zxvf libaio-libaio-0.3.113.tar.gz
 cd /project/libaio-libaio-0.3.113
 make prefix=/usr install
+make clean
 cd /project
 
 # install oneCCL: /project/oneCCL/build/_install
@@ -11,6 +12,7 @@ echo "Install oneCCL"
 cd /project/oneCCL
 mkdir build
 cd build
-cmake ..
+cmake .. -DCMAKE_INSTALL_PREFIX=/oneCCL -DCMAKE_BUILD_TYPE=Release
 make -j 1 install
+make clean
 cd /project
